7e3e6f2e9fb956744a766aff8ab1d61e18a45aa6
mariavictoriairazabal <111325196+mariavictoriairazabal@users.noreply.github.com>
2022-10-18 17:25:25 -0300

    Pre-entrega 2

--- codigo.js
+++ codigo.js
@@ -57,3 +57,58 @@ while(banda !="S"){
     }
     banda=prompt("Ingresa una banda y te diremos los albumes que tenemos en stock (S para finalizar)");
     }
+class Album{
+    constructor(titulo, precio, id){
+        this.titulo = titulo;
+        this.precio = Number(precio);
+        this.vendido = false;
+        this.id = id;
+        }
+        vender (){
+            this.vendido = true;
+        }
+        mostrar(){
+            return this.id+' '+this.titulo+' $'+this.precio+'/n';
+        }
+        textoCompra(){
+            return 'El álbum seleccionado es '+this.titulo+'/nSu precio es de $ '+this.precio;
+        }
+    }
+const albums = [];
+const album1 = new Album('Use Your Illusion I',1500,1);
+const album2 = new Album('Sticky Fingers The Rolling Stones',1800,2);
+const album3 = new Album('Abbey Road The Beatles',1700,3);
+const album4 = new Album('Youngblood 5 Seconds Of Summer',1400,4);
+const album5 = new Album('AM Arctics Monkeys',1650,5);
+const album6 = new Album('Stay On These Roads Aha',1800,6);
+const album7 = new Album('Nevermind Nirvana', 1415,7);
+const album8 = new Album('The Miracle Queen',1780,8);
+
+albums.push(album1, album2, album3, album4, album5, album6, album7, album8);
+
+function obtenerAlbums(){
+    let misAlbums = 'Ingrese el número de álbum que desea adquirir /n';
+    for (const album of albums){
+        misAlbums = misAlbums + album.mostrar();
+    }
+    return misAlbums;
+}
+let albumStock = false;
+
+ingresarAlbums();
+
+if(albumStock == false){
+    alert('No existe stock de el album ingresado');
+    ingresarAlbums();
+}
+
+function ingresarAlbums(){
+    let numeroIngresado=Number (prompt(obtenerAlbums));
+    albums.forEach(album => {
+        if(album.id===numeroIngresado){
+            albumStock=true;
+            alert(album.textoCompra());
+        }
+    }
+)}
+console.log(albums);
